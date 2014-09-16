9e48c40979e2c763d7dd0bda191aa8998c51a77c
Aditya Bhargava <adit@scribd.com>
2014-09-16 03:32:04 +0000

    this worked for me

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-OPENCV_JAR="/usr/local/Cellar/opencv/2.4.4a/share/OpenCV/java/opencv-244.jar"
+OPENCV_JAR="/home/adit/opencv_2.4.9_old/bin/opencv-249.jar"
 JAVA_FILE="helpers/org/opencv/LdLibraryLoader.java"
 javac -cp ${OPENCV_JAR} ${JAVA_FILE}
 
--- draw_features.rb
+++ draw_features.rb
@@ -1,16 +1,21 @@
 $CLASSPATH << "helpers"
-require 'java'
-require '/usr/local/Cellar/opencv/2.4.4a/share/OpenCV/java/opencv-244.jar'
+$CLASSPATH << "/home/adit/opencv_2.4.9_old/lib"
 
+require 'java'
+require '/home/adit/opencv_2.4.9_old/bin/opencv-249.jar'
 Java::org.opencv.LdLibraryLoader
 
 java_import org.opencv.core.Mat
+java_import org.opencv.core.Core
 java_import org.opencv.core.MatOfKeyPoint
 java_import org.opencv.highgui.Highgui
 java_import org.opencv.features2d.FeatureDetector
 java_import org.opencv.features2d.Features2d
+# java.lang.System.loadLibrary(Core::NATIVE_LIBRARY_NAME)
+
 
 src = Highgui.imread(ARGV.first)
+
 keypoints = MatOfKeyPoint.new
 detector = FeatureDetector.create(FeatureDetector::SIFT)
 detector.detect src, keypoints
